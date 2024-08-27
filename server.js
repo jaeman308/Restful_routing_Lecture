982599a0ca47f3d6df77f0302ced8c945e25ea6b
jborruel308@me.com <jborruel308@me.com>
2024-08-26 20:23:58 -0700

    add get and post route for /calculator

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
 const express = require('express')
 const app = express ()
 
+app.use(express.json());
+
+let total = 0;
+
+app.get('/calculator', (req, res) => {
+    res.status(200).json({total});
+});
+
+app.post('/calculator', (req, res) => {
+    const number = req.body.number; 
+    const operation = req.body.operation;
+    
+    if (operation === 'add') {
+        total += number; 
+    } else if (operation === 'subtract') {
+        total -= number; 
+    } else if (operation === 'multiply'){
+        total -= number;
+    } else if (operation === 'divide') {
+        total /= number; 
+    } else {
+        return res.status(400).json({error: 'invalid operation'})
+    }
+    res.status(200).json({total});
+
+});
+
+
+
+
+
 const PORT = 4000;
 app.listen(PORT, () => {
     console.log("listening on port 4000")
